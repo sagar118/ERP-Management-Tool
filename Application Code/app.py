f9fac4f079a0134ab016b5e22f25d815a1ccb97a
pushkaraj joshi <pushkaraj803@gmail.com>
2022-04-22 18:52:16 -0400

    Radio Button Added
    Added Radio Button instead of Sidebar

--- Application Code/app.py
+++ Application Code/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import home, blank#, metadata, data_visualize, redundant, inference # import your pages here
+from pages import home, home2#, metadata, data_visualize, redundant, inference # import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
@@ -12,8 +12,7 @@ st.title("DMQL Project")
 
 # Add all your applications (pages) here
 app.add_page("HOME", home.app)
-app.add_page("Home 2", blank.app)
-
+app.add_page("Home 2", home2.app)
 
 # The main app
 app.run()
