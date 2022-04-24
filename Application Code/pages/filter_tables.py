31f076543fb4b1699c8cb30d18a1bd0d83cb519d
pushkaraj joshi <pushkaraj803@gmail.com>
2022-04-24 15:54:50 -0400

    Select query completed
    Select query completed

--- Application Code/pages/filter_tables.py
+++ Application Code/pages/filter_tables.py
@@ -21,45 +21,54 @@ def app():
     data = pd.DataFrame(result, columns=colnames)
     st.dataframe(data)
 
-    cols_to_print = st.sidebar.multiselect("Select Columns to Print:", data.columns)
+    cols_to_print = st.sidebar.multiselect("Select Columns to Print (Nothing for all columns)", data.columns)
 
     cols_to_filter = st.sidebar.multiselect("Select Columns to Filter on", colnames)
     filters = {}
-    and_or_string = []
-    # if 
-    #     filters[cols_to_filter[0]] = st.sidebar.text_input('Enter your filter for {}'.format(cols_to_filter[0]))
-    # elif len(cols_to_filter) > 1:
-    
+    conditions = []
+    condition_values = []
+
     for column in cols_to_filter:
-        filters[column] = st.sidebar.text_input('Enter your filter for {} (eg: "< 10 or > 1 or = 5")'.format(column))
-        and_or_string.append(st.sidebar.text_input('AND OR after {} condition (leave blank if no other condition)'.format(column)))
-
-
+        conditions.append(st.sidebar.selectbox("Enter the condition for {}".format(column), ['=', '<', '>', '<=', '>=', "<>","like", "in", "between", "not like", "not in", "not between", "is null", "is not null", "is true", "is not true", "is false", "is not false"]))
+        filters[column] = st.sidebar.text_input('Enter your filter for {} (eg: "10")'.format(column))
+        condition_values.append(st.sidebar.selectbox('AND OR after {} condition (Note: Leave Blank if No Other Condition)'.format(column), ['Blank','AND', 'OR']))
     
-    # print('filters ', filters)
-    # print('and_or_string ', and_or_string)
-    st.button('Get Result', on_click=get_result(filters, table_name, and_or_string, cols_to_print))
+    form = st.form(key='my-form')
+    submit = form.form_submit_button('Get Result')
+    if submit:
+        get_result(filters, table_name, conditions, condition_values, cols_to_print)
 
-def get_result(filters, table_name, and_or_string, cols_to_print):
-    # query_builder = 
-    # st.dataframe(data)
+    # st.button('Get Result', on_click=get_result, args = (filters, table_name, and_or_string, cols_to_print))
+    # st.button('Get Result', on_click=get_result(filters, table_name, and_or_string, cols_to_print))
+
+def get_result(filters, table_name, conditions, condition_values, cols_to_print):
     query = ""
-    st.write(f'filters {filters} table_name {table_name} and_or_string {and_or_string} cols_to_print {cols_to_print}')
     if len(cols_to_print) == 0:
-        st.write('No columns selected')
+        st.write('No specific columns selected for display')
         query += "SELECT * FROM {}".format(table_name)
     else:
         query += "SELECT {} FROM {}".format(', '.join(cols_to_print), table_name)
 
-    counter_for_and_or_string = 0
-    if len(filters) > 0:
+    
+    # st.write('filters ',filters, 'table_name ', table_name, "conditions ",conditions, "condition_values ", condition_values, "cols_to_print ",cols_to_print)
+    if bool(filters):
+        keys, values = zip(*filters.items())
         query += " WHERE "
-        for key, value in filters.items():
-            if value != "":
-                if value[0] not in ['<', '>', '=', '!']:
-                    value = ' = ' + value
-                # print(filters.keys().tolist())
-                query += "{} {} {} ".format(key, value,and_or_string[counter_for_and_or_string])
-                
-                counter_for_and_or_string += 1
+        for key, value, condition, joining_value in zip(keys, values, conditions, condition_values):
+            if str(value).isdigit():
+                query +="{} {} {} ".format(key, condition, value)
+            else:
+                query +="{} {} '{}' ".format(key, condition, value)
+            if joining_value != 'Blank':
+                query += " {} ".format(joining_value)
     st.write(query)
+    try:
+
+        cur.execute(query)
+        colnames = [desc[0] for desc in cur.description]
+        result = cur.fetchall()
+        data = pd.DataFrame(result, columns=colnames)
+        st.dataframe(data)
+        # print('filters ',filters, 'table_name ', table_name, "conditions ",conditions, "condition_values ", condition_values, "cols_to_print ",cols_to_print)
+    except Exception as e:
+        print("Bad Query: ", e)
